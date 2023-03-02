0bb97943bb7eb499edfd537b523c720f88d7c4b8
Rafael Rezyapov <Rezograf@gmail.com>
2023-03-02 16:58:03 +0300

    add readToysFormJsonFile, getToysFromPriorityQueue

--- src/main/java/ru/rra/Main.java
+++ src/main/java/ru/rra/Main.java
@@ -9,54 +9,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-//        PriorityQueue<ToysShop> priorityQueue = new PriorityQueue<>(10); // компаратор в классе ToysShop
         PriorityQueue<ToysShop> priorityQueue = new PriorityQueue<>(10, new ToysComparator());
 
-        //jsonFileRead();
-
-//        priorityQueue.add(new ToysShop("1", "конструктор", 2));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-//        priorityQueue.add(new ToysShop("2", "робот", 2));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-//        priorityQueue.add(new ToysShop("2", "робот", 2));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-//        priorityQueue.add(new ToysShop("1", "конструктор", 2));
-//        priorityQueue.add(new ToysShop("3", "кукла", 6));
-
         Gson gson = new Gson();
-//        String userJson = gson.toJson(priorityQueue);
-//        jsonFileCreate(userJson);
-//        System.out.println(userJson);
-        String s = jsonFileRead();
-        System.out.println(s);
-        ToysShop[] ts1 = gson.fromJson(s, ToysShop[].class);
-        for (int i = 0; i < ts1.length; i++) {
-            priorityQueue.add(ts1[i]);
-        }
-        System.out.println(priorityQueue);
+        ToysShop[] toysShops = gson.fromJson(readToysFormJsonFile(), ToysShop[].class);
 
-        get(priorityQueue);
+        for (int i = 0; i < toysShops.length; i++) {
+            priorityQueue.add(toysShops[i]);
+        }
+
+        getToysFromPriorityQueue(priorityQueue);
     }
 
-    private static void jsonFileCreate(String userJson) {
-        try{
-            FileWriter fw = new FileWriter("ToysShop.json");
-            fw.write(userJson);
-            fw.flush();
-        }
-        catch (IOException e){e.printStackTrace();}
-    }
 
-    private static String jsonFileRead() {
+    private static String readToysFormJsonFile() {
         String data = "";
         try {
             File myObj = new File("ToysShop.json");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 data = data.concat(myReader.nextLine());
-//                System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -66,11 +38,22 @@ public class Main {
         return data;
     }
 
-    public static void get(PriorityQueue<ToysShop> pQ){
+    public static void getToysFromPriorityQueue(PriorityQueue<ToysShop> pQ){
+        String result;
         int pQSize = pQ.size();
-        for (int i = 0; i < pQSize; i++) {
-            System.out.println(pQ.poll().getId());
+        try {
+            FileWriter fw = new FileWriter("result.txt");
+            for (int i = 0; i < pQSize; i++) {
+                result = pQ.poll().getId();
+                System.out.println(result);
+                fw.write(result);
+            }
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
 
